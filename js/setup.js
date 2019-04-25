1e8bd8c354a5e3536ad4855a8a07ae12d1ff7cf0
Aleksei Arro <polsck@gmail.com>
2019-04-25 21:08:06 +0300

    Setup block position reset via popupClose

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,55 @@ Revealing Setup section
 */
 
 var setupBlock = document.querySelector('.setup');
+var userpic = setupBlock.querySelector('.upload');
+
+/*
+Setup block drag'n'drop
+*/
+
+var userpicClickPreventDefault = function (clickEvt) {
+  clickEvt.preventDefault();
+  userpic.removeEventListener('click', userpicClickPreventDefault);
+};
+
+var userpicMousedownHandler = function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {x: evt.clientX, y: evt.clientY};
+
+  var dragged = false;
+
+  var documentMousemoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: moveEvt.clientX - startCoords.x,
+      y: moveEvt.clientY - startCoords.y,
+    };
+
+    startCoords = {x: moveEvt.clientX, y: moveEvt.clientY};
+
+    setupBlock.style.top = (setupBlock.offsetTop + shift.y) + 'px';
+    setupBlock.style.left = (setupBlock.offsetLeft + shift.x) + 'px';
+  };
+
+  var documentMouseupHandler = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', documentMousemoveHandler);
+    document.removeEventListener('mouseup', documentMouseupHandler);
+
+    if (dragged) {
+      userpic.addEventListener('click', userpicClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', documentMousemoveHandler);
+  document.addEventListener('mouseup', documentMouseupHandler);
+};
+
+userpic.addEventListener('mousedown', userpicMousedownHandler);
 
 /*
 Initial data
@@ -260,6 +309,7 @@ var openPopup = function () {
 };
 var closePopup = function () {
   setupBlock.classList.add('hidden');
+  setupBlock.removeAttribute('style');
   document.removeEventListener('keydown', popupEscPressHandler);
   ourWizardCoat.removeEventListener('click', ourWizardCoatClickHandler);
   ourWizardEyes.removeEventListener('click', ourWizardEyesClickHandler);
