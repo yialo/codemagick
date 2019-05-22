904116b12b312acc284eac80260e8ff31060fc78
Aleksei Arro <polsck@gmail.com>
2019-05-23 01:58:45 +0300

    Wizards sort logics in progress: sort methods added

--- js/change-colors.js
+++ js/change-colors.js
@@ -1,10 +1,10 @@
 'use strict';
 
 (function () {
-  var wizard = window.domElements.setup.querySelector('.setup-player');
+  var player = window.domElements.setup.querySelector('.setup-player');
   var partMap = {
     'coat': {
-      element: wizard.querySelector('.wizard-coat'),
+      element: player.querySelector('.wizard-coat'),
       colors: [
         'rgb(101, 137, 164)',
         'rgb(241, 43, 107)',
@@ -14,17 +14,46 @@
         'rgb(0, 0, 0)',
       ],
       cssProperty: 'fill',
+      dbKey: 'colorCoat',
+      ratingMark: 2,
     },
     'eyes': {
-      element: wizard.querySelector('.wizard-eyes'),
+      element: player.querySelector('.wizard-eyes'),
       colors: ['black', 'red', 'blue', 'yellow', 'green'],
       cssProperty: 'fill',
+      dbKey: 'colorEyes',
+      ratingMark: 1,
     },
     'fireball': {
-      element: wizard.querySelector('.setup-fireball-wrap'),
+      element: player.querySelector('.setup-fireball-wrap'),
       colors: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
       cssProperty: 'backgroundColor',
+      dbKey: 'colorFireball',
+      ratingMark: 1,
     },
+  };
+
+  var setWizardsRating = function (part, newColor) {
+    window.backend.currentWizards.forEach(function (wizard) {
+      var rating = 0;
+      var dbKey = partMap[part].dbKey;
+      if (wizard[dbKey] === newColor) {
+        rating += partMap[part].ratingMark;
+      }
+      wizard.Rating[part] = rating;
+    });
+  };
+
+  var calculateTotalRating = function () {
+    window.backend.currentWizards.forEach(function (wizard) {
+      wizard.Rating.total = wizard.Rating.coat + wizard.Rating.eyes + wizard.Rating.fireball;
+    });
+  };
+
+  var sortSimilarWizards = function () {
+    window.backend.currentWizards.sort(function (left, right) {
+      return right.Rating.total - left.Rating.total;
+    });
   };
 
   var getWizardPartClickHandler = function (part) {
@@ -32,15 +61,18 @@
       var targetElement = partMap[part].element;
       var newColor = window.utilities
         .getRandomArrayElement(partMap[part].colors);
-      var input = wizard.querySelector('input[name=\"' + part + '-color\"]');
+      var input = player.querySelector('input[name=\"' + part + '-color\"]');
       targetElement.style[partMap[part].cssProperty] = newColor;
       input.value = newColor;
+      setWizardsRating(part, newColor);
+      calculateTotalRating();
+      sortSimilarWizards();
     };
   };
 
   var PARTS = ['coat', 'eyes', 'fireball'];
-  var partHandlers = PARTS.map(function (item) {
-    return getWizardPartClickHandler(item);
+  var partHandlers = PARTS.map(function (part) {
+    return getWizardPartClickHandler(part);
   });
 
   window.changeColors = {
--- js/similar-wizards-render.js
+++ js/similar-wizards-render.js
@@ -3,6 +3,12 @@
 (function () {
   var AMOUNT_OF_SLOTS = 4;
   var setup = window.domElements.setup;
+  var container = setup.querySelector('.setup-similar');
+
+  /*
+    TODO: оптимизировать рендеринг. Отрисовка 4 блоков волшебников
+    должна производиться один раз, далее - только изменение цветов.
+  */
 
   var createSimilarWizard = function (wizardData) {
     var wizardElement = document.querySelector('#similar-wizard-template')
@@ -15,25 +21,34 @@
     nameElement.textContent = wizardData.name;
     coatElement.style.fill = wizardData.colorCoat;
     eyesElement.style.fill = wizardData.colorEyes;
-
     return wizardElement;
   };
 
   var addSimilarWizards = function (wizardsData) {
-    var container = setup.querySelector('.setup-similar');
-    var wizardsList = container.querySelector('.setup-similar-list');
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < AMOUNT_OF_SLOTS; i += 1) {
-      var newWizard = createSimilarWizard(wizardsData[i]);
-      fragment.appendChild(newWizard);
-    }
-
-    wizardsList.appendChild(fragment);
+    window.backend.initialWizards = wizardsData;
+    window.backend.currentWizards = wizardsData.slice();
+    window.backend.currentWizards.forEach(function (it) {
+      it.Rating = {};
+    });
+    window.similarWizardsRender.renewSimilarWizards(wizardsData);
     container.classList.remove('hidden');
   };
 
   window.similarWizardsRender = {
+    renewSimilarWizards: function (wizardsData) {
+      var wizardsList = container.querySelector('.setup-similar-list');
+      var fragment = document.createDocumentFragment();
+
+      while (wizardsList.firstChild) {
+        wizardsList.removeChild(wizardsList.firstChild);
+      }
+
+      for (var i = 0; i < AMOUNT_OF_SLOTS; i += 1) {
+        var newWizard = createSimilarWizard(wizardsData[i]);
+        fragment.appendChild(newWizard);
+      }
+      wizardsList.appendChild(fragment);
+    },
     showErrorMessage: function (errMessage) {
       var node = document.createElement('span');
       node.style.position = 'absolute';
