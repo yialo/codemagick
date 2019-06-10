86da53e2a0238eaff59b003f87562c03ae2e1ca8
Aleksei Arro <aaa2@softingmsk.ru>
2019-06-10 11:16:48 +0300

    backend.js: xhr wrapper added

--- js/backend.js
+++ js/backend.js
@@ -1,67 +1,50 @@
 'use strict';
 
 (function () {
+  window.backend = {isDownloaded: false};
   var Status = {SUCCESS: 200, NOT_FOUND: 404};
   var TIMEOUT = 5000;
   var URL = 'https://js.dump.academy/code-and-magick';
-  window.backend = {isDownloaded: false};
 
-  /*
-    TODO: определить xhr через расширение прототипа XMLHttpRequest
-  */
-
-  window.backend.upload = function (data, successHandler, errorHandler) {
+  var getXhr = function (onSuccessCallback, onErrorCallback) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.timeout = TIMEOUT;
-
     xhr.addEventListener('load', function () {
-      if (xhr.status === Status.SUCCESS) {
-        successHandler(xhr.response);
-      } else {
-        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      switch (xhr.status) {
+        case Status.SUCCESS:
+          onSuccessCallback(xhr.response);
+          break;
+        case Status.NOT_FOUND:
+          onErrorCallback('Неверный адрес');
+          break;
+        default:
+          onErrorCallback(
+              'Статус ответа: ' + xhr.status + ' ' + xhr.statusText
+          );
       }
     });
-
     xhr.addEventListener('error', function () {
-      errorHandler('Ошибка сетевого соединения');
+      onErrorCallback('Ошибка сетевого соединения');
     });
-
     xhr.addEventListener('timeout', function () {
-      errorHandler('Данные не удалось получить в течение ' + xhr.timeout / 1000 + ' секунд');
+      onErrorCallback('Данные не удалось получить в течение ' + xhr.timeout / 1000 + ' секунд');
     });
+    return xhr;
+  };
 
+  window.backend.upload = function (data, successHandler, errorHandler) {
+    var xhr = getXhr(successHandler, errorHandler);
     xhr.open('POST', URL);
     xhr.send(data);
   };
 
   window.backend.download = function (successHandler, errorHandler) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.timeout = TIMEOUT;
-
-    xhr.addEventListener('load', function () {
-      switch (xhr.status) {
-        case Status.SUCCESS:
-          window.backend.isDownloaded = true;
-          successHandler(xhr.response);
-          break;
-        case Status.NOT_FOUND:
-          errorHandler('Неверный адрес');
-          break;
-        default:
-          errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      errorHandler('Ошибка сетевого соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      errorHandler('Данные не удалось получить в течение ' + xhr.timeout / 1000 + ' секунд');
-    });
-
+    var successHandlerWrapper = function (response) {
+      window.backend.isDownloaded = true;
+      successHandler(response);
+    };
+    var xhr = getXhr(successHandlerWrapper, errorHandler);
     xhr.open('GET', URL + '/data');
     xhr.send();
   };
